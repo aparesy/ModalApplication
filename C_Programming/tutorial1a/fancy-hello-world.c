40539c5728c30e2ec9f742a0ec5af82c526df3c4
Paresy a <paresya888@gmail.com>
2025-11-30 22:01:09 +0100

    Created repo

---
+++ C_Programming/tutorial1a/fancy-hello-world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include "fancy-hello-world.h"
+
+int main(void) {
+    char name[60];
+    char output[60];
+    fgets(name, 51, stdin);
+    hello_string(name, output);     
+    printf("Hello world, hello ");
+    printf("%s", output);
+    return 0;
+}
+
+void hello_string(char* name, char* output){
+    strcpy(output, name);
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ModalApplication
 Some projects for a distributed application course I'm taking
+
+The first few weeks are dedicated to learning the basics of network programming and architecture, as well as concurent programmng in C. The culmination of the course is a final project that accounts for most of the grade.
